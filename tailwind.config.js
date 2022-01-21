d7f0f77403a43129618d4107289411ce91db563b
Woodlouse <m.mamontov-2024@kpi.ua>
2022-01-21 22:54:43 +0200

    header via bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 module.exports = {
   prefix: '',
+  mode: 'jit',
   purge: {
+    enabled: false,
     content: [
       './src/**/*.{html,ts}',
     ]
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wood-blue': '#0652C5',
+        'wood-pink': '#D4418E'
+      }
+    },
   },
   variants: {
     extend: {},
